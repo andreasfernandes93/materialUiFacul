27194a1d6d2ddc1d0b1396472482c0c536c154b3
Andreas Fernandes <andreas.sf93@gmail.com>
2024-06-02 17:17:22 -0300

    refactor: update theme to dark mode

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import Header from "./components/header";
+import ListarTarefa from "./pages/tarefa/ListarTarefa";
+
+import { ThemeProvider, createTheme } from '@mui/material/styles';
+import CssBaseline from '@mui/material/CssBaseline';
+
+const darkTheme = createTheme({
+  palette: {
+    mode: 'dark',
+  },
+});
+
+function App() {
+  return (
+    <ThemeProvider theme={darkTheme}>
+    <div className="App">
+      <Header />
+      <ListarTarefa />
+    </div>
+    </ThemeProvider>
+  );
+}
+
+export default App;
